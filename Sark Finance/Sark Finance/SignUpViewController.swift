d7654bff2b4faa8c638206d5694ae44cee4a2e75
kshu2011 <uw.kshu@gmail.com>
2022-03-17 11:33:08 -0500

    outlets for text fields added to xcode.

---
+++ Sark Finance/Sark Finance/SignUpViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignUpViewController.swift
+//  Sark Finance
+//
+//  Created by Kevin on 3/17/22.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
