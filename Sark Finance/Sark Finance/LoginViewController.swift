57072818bb2392d17f14c646aa4b635beeb23781
sajidahw <88634981+sajidahw@users.noreply.github.com>
2022-03-17 23:16:21 -0700

    created login screen with view controller, added logo to assets so it launches from mobile home screen, added an empty navigation controller so it opens up when sign up button from login screen is clicked on, pasted in login parse info for login button.

---
+++ Sark Finance/Sark Finance/LoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  LoginViewController.swift
+//  Sark Finance
+//
+//  Created by Sajidah Wahdy on 3/17/22.
+//
+
+import UIKit
+import AlamofireImage
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func onLogin(_ sender: Any) {
+        print("Clicked on Login button, need to update Parse for app")
+        let username = usernameField.text!
+        let password = passwordField.text!
+
+        PFUser.logInWithUsername(inBackground: username, password: password)
+          {
+          (user, error) in
+          if user != nil {
+            self.performSegue(withIdentifier: "loginSegue", sender: nil)
+          } else {
+            print("Error: \(String(describing: error?.localizedDescription))")
+          }
+        }
+    }
+    
+    @IBAction func onSignup(_ sender: Any) {
+        print("Clicked on Sign up button")
+        print("Should direct to Sign up Page")
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
