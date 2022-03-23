7a38c8b89270ea56ae730efbd11b75f63af89efb
sajidahw <88634981+sajidahw@users.noreply.github.com>
2022-03-22 22:23:41 -0700

    Added swift files for chatCell and chatTableViewController with basic code for submit button, outlet for textfield

---
+++ Sark Finance/Sark Finance/chatCell.swift
@@ -0,0 +1,23 @@
+//
+//  chatCell.swift
+//  Sark Finance
+//
+//  Created by Sajidah Wahdy on 3/22/22.
+//
+
+import UIKit
+
+class chatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
